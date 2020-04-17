142112e4e81ca4a0170b71461e185237cd2f6b78
Luming Hao <lh00000000@gmail.com>
2020-04-17 04:16:21 -0400

    auth as middle

--- src/asComposer.js
+++ src/asComposer.js
@@ -1,30 +1,37 @@
 const validate = require("./validity")
 const router = require("express").Router()
 const {composers, opuses} = require("./mongo")
+const _ = require("lodash")
 
+router.use((req, res, next) => {
+    let apiKey = _.get(req.headers.authorization.split(" "),  1, "")
+    composer = await composers()
+        .findOne({ apiKey  })
+    if (composer === undefined) {
+        res.status(403)
+        res.end()
+    } else {
+        res.locals.composer = composer
+        res.locals.composerId = composer._id.toString()
+    }
+
+    next()
+})
 
 router.post("/opus", async (req, res, next) => {
     try {
-        composer = await composers()
-            .findOne({ apiKey: req.headers.authorization.split(" ")[1] })
-
-        if (composer === undefined) {
-            res.status(403)
-            res.end()
-        }
-
         if (validate("opus", req.body)) {
 
             let newOpus = {
                 events: req.body.events,
                 submitted: new Date(),
-                composerId: composer._id.toString(),
+                composerId: res.locals.composerId,
                 status: "AVAILABLE",
             }
 
             let confirm = await opuses().insertOne(newOpus)
             res.json({
-                id: confirm.insertedId,
+                id: confirm.insertedId.toString(),
                 submitted: newOpus.submitted,
                 events: {
                     count: req.body.events.length
